4dad0270a024fc059003809315ecf1a64ccb2815
minh son <minhson.uit@gmail.com>
2020-05-14 16:00:04 +0700

    Add seeding data

--- eShopSolution.Data/Migrations/20200514083111_SeedData.Designer.cs
+++ eShopSolution.Data/Migrations/20200514083111_SeedData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShopSolution.Data.EF;
 
 namespace eShopSolution.Data.Migrations
 {
     [DbContext(typeof(EShopDbcontext))]
-    partial class EShopDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20200514083111_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
