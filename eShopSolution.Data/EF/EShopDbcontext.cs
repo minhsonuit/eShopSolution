c0bda191e8fe4a13e5ed6d1634a70f878a9b63e8
minh son <minhson.uit@gmail.com>
2020-05-19 16:54:22 +0700

    update database

--- eShopSolution.Data/EF/EShopDbcontext.cs
+++ eShopSolution.Data/EF/EShopDbcontext.cs
@@ -1,6 +1,8 @@
 ﻿using eShopSolution.Data.Configurations;
 using eShopSolution.Data.Entities;
 using eShopSolution.Data.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Text;
 
 namespace eShopSolution.Data.EF
 {
-   public class EShopDbcontext:DbContext
+   public class EShopDbcontext:IdentityDbContext<AppUser,AppRole,Guid>
     {
         public EShopDbcontext(DbContextOptions options):base(options)
         {
@@ -32,6 +34,16 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            modelBuilder.ApplyConfiguration(new AppRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x=> new { x.UserId,x.RoleId});
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x=>x.UserId);
+
+            //IdentityRoleClaim<TKey>, IdentityUserToken
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x=>x.UserId);
             //Seed data
             modelBuilder.Seed();
 
